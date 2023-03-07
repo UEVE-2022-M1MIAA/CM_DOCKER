8b2bbfd3c01ba588de5105d9421d2bbc31192f0b
SMorais <sebastien.morais@cea.fr>
2023-03-07 15:15:02 +0100

    Mise a jour LAMP pour utilisation des volumes

--- stack_lamp/db/articles.sql
+++ stack_lamp/db/articles.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS test CHARACTER SET utf8;
-CREATE USER 'test'@'localhost' IDENTIFIED BY 'test';
+CREATE USER IF NOT EXISTS 'test'@'localhost' IDENTIFIED BY 'test';
 GRANT ALL PRIVILEGES ON test.* TO 'test'@'localhost';
 
 
@@ -14,8 +14,3 @@ CREATE TABLE IF NOT EXISTS articles (
     PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-INSERT INTO articles (title, author, content) VALUES
-("Mon premier article", 'Moi!', "Ceci est mon premier article, il va parler de l'utilisation de Docker pour les debutants"),
-("Docker pour les inities", 'Toujours moi!', "Ceci est la suite de mon premier article, il s'adresse a un publique ayant deja des connaissances sur Docker"),
-("Docker pour les veterans", 'Devine !', "Ceci est mon dernier article, il s'adresse au gens ayant deja utilise Docker dans un contexte professionnel")
